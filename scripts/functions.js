d40bb04b1063ca60cf6a7bc88cc58100c9a4579b
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2024-10-17 18:45:13 -0400

    added drawLine to functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,26 +5,29 @@
 
 class Functions {
     static calculateXY2Angle(xVelocity, yVelocity) {
-        // Calculate angle in radians
         const angleInRadians = Math.atan2(yVelocity, xVelocity);
-
-        // Convert to degrees
         const angleInDegrees = angleInRadians * (180 / Math.PI);
-
-        // Normalize the angle to be between 0 and 360 degrees
-        return (angleInDegrees + 360) % 360; // Ensures the angle is positive
+        return (angleInDegrees + 360) % 360; // Ensure the angle is positive
     }
 
     static calculateAngle2XY(angle, decimals = 4) {
         const radians = angle * (Math.PI / 180);
         const x = (Math.cos(radians)).toFixed(decimals);
         const y = (Math.sin(radians)).toFixed(decimals);
-
         return { x: parseFloat(x), y: parseFloat(y) };
     }
 
     static randomGenerator(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    static drawLine(ctx, x1, y1, x2, y2, lineWidth = 5, strokeColor = 'white') {
+        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = strokeColor; // Default to white if no color is provided
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
     }
 }
 
