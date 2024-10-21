a815708325d77d4b65c90eb18264e8501aed0484
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2024-10-21 11:53:31 -0400

    reduce code footprint

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -92,13 +92,11 @@ class CanvasUtils {
     static clickFullscreen(ctx) {
         if (!Fullscreen.isFullScreen) {
             // Set up the text properties using global variables or default values
-            const text = window.fullscreenText || 'Click here to enter fullscreen';
-            const font = window.fullscreenFont || '40px Arial';
-            const color = window.fullscreenColor || 'white';
-            ctx.fillStyle = color; // Set text color
-            ctx.font = font; // Set font size and family
+            ctx.fillStyle = window.fullscreenColor || 'white'; // Set text color
+            ctx.font = window.fullscreenFont || '40px Arial';; // Set font size and family
             
             // Measure the width of the text "Click here to enter fullscreen"
+            const text = window.fullscreenText || 'Click here to enter fullscreen';
             const textWidth = ctx.measureText(text).width; // Get the width of the text in pixels
             const textX = (window.gameAreaWidth - textWidth) / 2; // Center the text horizontally
             const textY = window.gameAreaHeight * (4 / 5); // Position the text vertically
