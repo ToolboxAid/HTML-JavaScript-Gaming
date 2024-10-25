ccbd5179f2345100580620941c51ef45d2e738c2
Mr. Q <97194984+ToolboxAid@users.noreply.github.com>
2024-10-25 19:26:31 -0400

    updated sheild to keep in memory version.

--- Space Invaders/sheild.js
+++ Space Invaders/sheild.js
@@ -5,47 +5,43 @@
 
 import ObjectStatic from '../scripts/objectStatic.js';
 import { spriteConfig } from './global.js';
+import CanvasUtils from '../scripts/canvas.js';
 
 // New 22x13 pixel image for the shield (single frame)
-const frame = [
-    "0000111111111111111110000",
-    "0001111111111111111111000",
-    "0011111111111111111111100",
-    "0111111111111111111111110",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111111111111111111111",
-    "1111111110000000111111111",
-    "1111111100000000011111111",
-    "1111111000000000001111111",
-    "1111110000000000000111111",
-    "1111110000000000000111111",
-    "1111110000000000000111111",
-];
 
 class Shield extends ObjectStatic {
+
+    static frame = [
+        "0000111111111111111110000",
+        "0001111111111111111111000",
+        "0011111111111111111111100",
+        "0111111111111111111111110",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111111111111111111111",
+        "1111111110000000111111111",
+        "1111111100000000011111111",
+        "1111111000000000001111111",
+        "1111110000000000000111111",
+        "1111110000000000000111111",
+        "1111110000000000000111111",
+    ];
+
     constructor(x, y) {
         super(x, y); // Call the parent class constructor
-        this.frame = frame; // Assign the single frame to this object
+        this.frame = Shield.frame; // Assign the single frame to this object
         this.pixelSize = window.pixelSize;
     }
 
     // Method to draw the shield
     draw(ctx) {
-        for (let row = 0; row < this.frame.length; row++) {
-            for (let col = 0; col < this.frame[row].length; col++) {
-                const pixel = this.frame[row][col];
-                const color = pixel === '1' ? spriteConfig.sheildColor : 'transparent';
-                ctx.fillStyle = color;
-                ctx.fillRect((col * this.pixelSize) + this.x, (row * this.pixelSize) + this.y, this.pixelSize, this.pixelSize);
-            }
-        }
+        CanvasUtils.spriteDrawer(ctx, this.x, this.y, this.frame, this.pixelSize);
     }
 }
 
