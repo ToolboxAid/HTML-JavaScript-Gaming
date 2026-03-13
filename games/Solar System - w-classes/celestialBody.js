37c09afffdd923401282aa46309a4debc2ca9132
Mr. Q <97194984+ToolboxAid@users.noreply.github.com>
2026-03-12 21:03:57 -0400

    test

--- games/Solar System - w-classes/celestialBody.js
+++ games/Solar System - w-classes/celestialBody.js
@@ -6,6 +6,7 @@
 import { canvasConfig } from '../Solar System - w-classes/global.js'; // Import canvasConfig
 import CanvasUtils from '../../engine/canvas.js';
 import ObjectDynamic from '../../engine/objects/objectDynamic.js'; // Import ObjectDynamic
+ 
 
 /**
  * Represents a celestial body in a solar system, such as a planet or star/sun.
