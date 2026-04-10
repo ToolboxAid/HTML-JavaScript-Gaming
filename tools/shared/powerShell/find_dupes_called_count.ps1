c9fe6e58ced43f8b530122e06427e98df7105702
Mr. Q <97194984+ToolboxAid@users.noreply.github.com>
2026-04-09 21:38:33 -0400

    find dupes order by count

---
+++ tools/shared/powerShell/find_dupes_called_count.ps1
@@ -0,0 +1,28 @@
+# 1. Get all .js files recursively
+# 2. Look for function patterns
+# 3. Group them by the text found
+# 4. Filter for groups with more than 1 occurrence
+# 5. Sort by duplicate count, lowest to highest
+
+# .\find_dupes_called.ps1 > found_dupes_called.txt
+# .\find_dupes_called.ps1 | Out-File -FilePath "found_dupes_called.txt" -Encoding utf8
+
+# Goes up 3 levels to reach HTML-JavaScript-Gaming from tools\shared\powerShell\
+# Get the root path (3 levels up)
+$rootPath = Resolve-Path "$PSScriptRoot\..\..\..\"
+
+Get-ChildItem -Path $rootPath -Recurse -Filter *.js |
+    Select-String -Pattern "function\s+[a-zA-Z0-9_]*\(", "[a-zA-Z0-9_]*\s*=\s*function", "[a-zA-Z0-9_]*:\s*function" |
+    Group-Object Line |
+    Where-Object { $_.Count -gt 1 } |
+    Sort-Object Count |
+    ForEach-Object {
+        "($($_.Count)) Duplicate line: $($_.Name.Trim())"
+
+        $_.Group | ForEach-Object {
+            # Replace the absolute root path with a single backslash
+            $relativePath = $_.Path -replace [regex]::Escape($rootPath), ""
+            "   -> Line $($_.LineNumber): \$relativePath"
+        }
+        ""
+    }
