bdeb9bd7249345cad87f2928caa6948804b7c000
DavidQ <qbytes.dq@gmail.com>
2026-05-20 11:02:17 -0400

    Correct alias collision

--- games/Asteroids/utils/math.js
+++ games/Asteroids/utils/math.js
@@ -4,12 +4,21 @@ David Quesenberry
 03/22/2026
 math.js
 */
-import { wrap as sharedWrap } from '../../../src/shared/utils/mathUtils.js';
-
 export { randomRange } from '../../../src/shared/utils/mathUtils.js';
 
 export const TAU = Math.PI * 2;
 
 export function wrap(value, max) {
-  return sharedWrap(value, 0, max);
+  const low = Math.min(0, max);
+  const high = Math.max(0, max);
+  const span = high - low;
+  if (span === 0) {
+    return low;
+  }
+
+  if (value >= low && value <= high) {
+    return value;
+  }
+
+  return ((((value - low) % span) + span) % span) + low;
 }
